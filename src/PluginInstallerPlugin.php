355ec0da469da4399dc2228baee911fcffdaab0e
Jamiel <20553479+jamiel-sharief@users.noreply.github.com>
2019-05-31 11:18:28 +0200

    Changed to composer plugin

---
+++ src/PluginInstallerPlugin.php
@@ -0,0 +1,15 @@
+<?php
+namespace Origin\Composer;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class PluginInstallerPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installer = new PluginInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($installer);
+    }
+}
